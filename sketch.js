b22fcb23dc45833bfb31c8bd8a82204fec551a23
elputnam <elputnam@inaction.ie>
2021-07-22 17:50:24 +0100

    Minimized design and added text

--- sketch.js
+++ sketch.js
@@ -1,46 +1,63 @@
 const DefaultTwists = 0.5;
 
 let rad;
-let v;
+let v = 100;
 let turn = 0;
 let num;
-// let twists = 1;
+let twists = 2;
 let twistSlider;
+let myFont;
+function preload() {
+  myFont = loadFont('assets/MajorMonoDisplay-Regular.ttf');
+}
 
 function setup() {
 	createCanvas(windowWidth, windowHeight, WEBGL);
 	twistSlider = createSlider(0, 10, DefaultTwists, 0);
 	twistSlider.position(10, 10);
+
 	colorMode(HSB, 360, 100, 100, 100);
 	rad = height * 0.3;
-	v = height * 0.1;
+	// v = height * 0.1;
 	num = height * 0.5;
+	textFont(myFont);
 	frameRate(30)
 }
 
 function draw() {
-	background(40, 20, 100);
-	orbitControl(2, 1, 0.05);
+	background(200);
+	push();
+	translate(-width*.5, -height*.45)
+	textSize(32);
+	fill(0);
+	text(twists, 10, 30, 50, 50);
+	pop();
+
+	//orbitControl(2, 1, 0.05);
 	//background(10);
 	lights();
+  
 	push();
 	//noStroke();
 	rotateX(turn);
-  rotateY(turn);
-  rotateZ(turn);
+    rotateY(turn);
+    rotateZ(turn);
   drawTrack(100, rad, v);
-	drawEdges(100, rad, v);
+	//drawEdges(100, rad, v);
 	turn += 0.003
 	pop();
 }
 
 function drawTrack(steps, rad, v) {
-	beginShape(TRIANGLE_STRIP);
-	fill(0);
-	strokeWeight(2);
-	stroke(255);
-	let twists = twistSlider.value();
+	beginShape(LINE_STRIP);
+	noFill();
+	//fill(200, 100, 100);
+  // noStroke();
+ twists = twistSlider.value();
   print(twists);
+  stroke(0);
+  //fill(255, 50);
+	strokeWeight(5);
 	for (let step = 0; step < (steps + 1); step += 1) {
 		let u = step * TAU / steps;
 		let x = (rad - v * cos(twists * u)) * cos(u);
@@ -56,7 +73,7 @@ function drawTrack(steps, rad, v) {
 }
 
 function drawEdges(steps, rad, v) {
-	let twists = twistSlider.value();
+	twists = twistSlider.value();
   for (let step = 0; step < (steps + 1); step += 1) {
 		fill(260, random(100), 100);
 		stroke(0)
